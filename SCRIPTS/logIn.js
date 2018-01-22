b7a3b8f47faacc2f248af7fbae134e08d77c4431
joxterboi <joa123kim456@gmail.com>
2018-01-22 10:49:38 +0100

    Added multiple logins to site

--- SCRIPTS/logIn.js
+++ SCRIPTS/logIn.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 	function logIn() {
-		var uid = "joakim";
-		var pswrd = "123";
-		if ($("#uid").val() == uid && $("#pswrd").val() == pswrd) {
-			localStorage.setItem("access", "true");
-			window.location.replace("hem.html");
-		} else {
-			console.log("Wrong password or username!");
+		var uid = ["joakim", "hektor", "andreas"];
+		var pswrd = ["123", "456", "789"];
+
+		for (var i = 0; i < uid.length; i++) {
+			if ($("#uid").val() == uid[i] && $("#pswrd").val() == pswrd[i]) {
+				localStorage.setItem("access", "true");
+				window.location.replace("hem.html");
+			} else {
+				console.log("Wrong password or username!");
+			}
 		}
+
 	}
 
 	$("#pswrd").keypress(function(e) {
