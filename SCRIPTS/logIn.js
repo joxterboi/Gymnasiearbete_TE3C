76c26ef75b0a73803487835a65c965a8d93260b7
joxterboi <joa123kim456@gmail.com>
2018-01-22 12:14:30 +0100

    Added notification when wrong password

--- SCRIPTS/logIn.js
+++ SCRIPTS/logIn.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
+	localStorage.setItem("access", "false");
+	$("#wrongPswrd").hide();
 	function logIn() {
 		var uid = ["joakim", "hektor", "andreas"];
 		var pswrd = ["123", "456", "789"];
-
 		for (var i = 0; i < uid.length; i++) {
 			if ($("#uid").val() == uid[i] && $("#pswrd").val() == pswrd[i]) {
 				localStorage.setItem("access", "true");
 				window.location.replace("hem.html");
 			} else {
-				console.log("Wrong password or username!");
+				$("#pswrd").val("")
+				$("#wrongPswrd").show();
 			}
 		}
-
 	}
-
 	$("#pswrd").keypress(function(e) {
 		if (e.which == 13) {
 			logIn();
