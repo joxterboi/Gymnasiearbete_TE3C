240f10dc49a97072c4e1f6b6e31f0749373d5ae2
joxterboi <joa123kim456@gmail.com>
2018-01-23 08:27:09 +0100

    Fixed usernmaes

--- SCRIPTS/logIn.js
+++ SCRIPTS/logIn.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	localStorage.setItem("access", "false");
 	$("#wrongPswrd").hide();
 	function logIn() {
-		var uid = ["JOKJE001", "HEBY001", "ANER001"];
+		var uid = ["JOKJE001", "HEBYF001", "ANERI023"];
 		var pswrd = ["123", "456", "789"];
 		for (var i = 0; i <= uid.length; i++) {
 			if ($("#uid").val().toUpperCase() == uid[i] && $("#pswrd").val() == pswrd[i]) {
