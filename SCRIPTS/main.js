5d09004850c69038baca45c959cab5f62c2b3f7b
joxterboi <joa123kim456@gmail.com>
2018-01-27 22:24:13 +0100

    Finished absence page

--- SCRIPTS/main.js
+++ SCRIPTS/main.js
@@ -61,20 +61,38 @@ $("#addAbsence").click(function(){
 	$("#addAbsencePopUp").show();
 })
 // Sparar frånvaron
-startTime = JSON.parse(localStorage.getItem('startTime') || "[]");
-
 var saveAbsence = function () {
-  var absence = $("#startTime").val();
-  if (absence != "") {  	
-	  startTime.push(absence);
-	  localStorage.setItem('startTime', JSON.stringify(startTime));
-	  $("#startTime").val("");
-	  cancelAbsence();
-	  showAbsence();
-  }
+	var absence = $("#startTime").val();
+	if (absence && $("#stopTime").val() && $("#date").val() != "") {
+		if (absence.match(/[0-9, :]/i) && $("#stopTime").val().match(/[0-9, :]/i) && $("#date").val().match(/[0-9, /]/i)) {
+			if (absence.length = 5 && $("#stopTime").val().length <= 5 && $("#stopTime").val().length >= 5 && $("#date").val().length > 2 && $("#date").val().length < 6) {
+				  if (absence != "") {  	
+					  startTime.push(absence);
+					  stopTime.push($("#stopTime").val());
+					  date.push($("#date").val());
+					  localStorage.setItem('startTime', JSON.stringify(startTime));
+					  localStorage.setItem('stopTime', JSON.stringify(stopTime));
+					  localStorage.setItem('date', JSON.stringify(date));
+					  $("#startTime").val("");
+					  $("#stopTime").val("");
+					  $("#date").val("");
+					  cancelAbsence();
+					  showAbsence();
+					}	
+			} else {
+				alert("Var snäll och fyll i ett tillåtet format!\n(hh:mm)\n(hh:mm)\n(dd/mm)");
+			}
+	} else {
+		alert("Var snäll och bara fyll i nummer i fälten!")
+	}
+  } else {
+		alert("Var snäll och fyll i alla fält innan du klickar på spara!");
+	}
 }
 function rensa() {
 	localStorage.setItem('startTime', "[]");
+	localStorage.setItem('stopTime', "[]");
+	localStorage.setItem('date', "[]");
 	location.reload();
 }
 // Tar bort frånvarorutan
@@ -84,12 +102,14 @@ function cancelAbsence() {
 // View repported absence
 function showAbsence() {
 	$("#absenceCards").text("");
-	startTime = JSON.parse(localStorage.getItem('startTime'));
+	startTime = JSON.parse(localStorage.getItem('startTime') || "[]");
+	stopTime = JSON.parse(localStorage.getItem('stopTime') || "[]");
+	date = JSON.parse(localStorage.getItem('date') || "[]");
 	for (i = 0; i < startTime.length; i++) {
 	  $('<div class="absenceCard card" />').html(
 	  	"<h1>" + "Från: " + "<span>" + startTime[i] + "</span>" + "</h1>" + 
-	  	"<h1>" + "Till: " + "<span>" + startTime[i] + "</span>" + "</h1>" + 
-	  	"<h1>" + "Datum: " + "<span>" + startTime[i] + "</span>" + "</h1>"
+	  	"<h1>" + "Till: " + "<span>" + stopTime[i] + "</span>" + "</h1>" + 
+	  	"<h1>" + "Datum: " + "<span>" + date[i] + "</span>" + "</h1>"
 	  	).appendTo('#absenceCards');
 	}
 }
