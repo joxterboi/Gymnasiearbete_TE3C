7cc8a6a7777373c3d2513cd2a032533436b4be58
joxterboi <joa123kim456@gmail.com>
2018-01-27 15:41:47 +0100

    Fixed capitalization so it works on a webserver

--- scripts/load.js
+++ scripts/load.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	$("header").load("../includes/header.html");
-	$("footer").load("../includes/footer.html");
+	$("header").load("../INCLUDES/header.html");
+	$("footer").load("../INCLUDES/footer.html");
 	var pageTitle = $("#title").html();
 	$(document).attr("title", pageTitle);
 });
