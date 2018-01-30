f0ad773b67c17f749ea23061bd13c7c1d750eae4
joxterboi <joa123kim456@gmail.com>
2018-01-30 11:08:42 +0100

    Fixed login button

--- SCRIPTS/logIn.js
+++ SCRIPTS/logIn.js
@@ -9,28 +9,27 @@ $(document).ready(function() {
 		checkt();
 	}
 	
-	function logIn() {
-		var uid = ["JOKJE001", "HEBYF001", "ANERI023"];
-		var pswrd = ["123", "456", "789"];
-		for (var i = 0; i <= uid.length; i++) {
-			if ($("#uid").val().toUpperCase() == uid[i] && $("#pswrd").val() == pswrd[i]) {
-				localStorage.setItem("access", "true");
-				window.location.replace("hem.html");
-				localStorage.setItem("uid", i);
-				break;
-			} else if (i >= uid.length){
-				$("#pswrd").val("");
-				$("#wrongPswrd").show();
-			}
-		}
-	}
 	$("#pswrd").keypress(function(e) {
 		if (e.which == 13) {
 			logIn();
 		}
 	});
 });
-$("#logIn").click(logIn());
+function logIn() {
+	var uid = ["JOKJE001", "HEBYF001", "ANERI023"];
+	var pswrd = ["123", "456", "789"];
+	for (var i = 0; i <= uid.length; i++) {
+		if ($("#uid").val().toUpperCase() == uid[i] && $("#pswrd").val() == pswrd[i]) {
+			localStorage.setItem("access", "true");
+			window.location.replace("hem.html");
+			localStorage.setItem("uid", i);
+			break;
+		} else if (i >= uid.length){
+			$("#pswrd").val("");
+			$("#wrongPswrd").show();
+		}
+	}
+}
 function checkt() {
 		$(".checkbox").toggleClass("clicked");
 	}
